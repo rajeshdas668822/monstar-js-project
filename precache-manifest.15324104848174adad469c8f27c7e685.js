3d346d11f663a4ddcbbb88834dda349f915ed74f
rajeshdas6688 <rdas@numerix.com>
2020-10-11 16:53:11 +0800

    Updates

---
+++ precache-manifest.15324104848174adad469c8f27c7e685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bb8fe73445cfac35d131442b797eff",
+    "url": "/monstar-js-project/index.html"
+  },
+  {
+    "revision": "bb687df2cc76d85de4f0",
+    "url": "/monstar-js-project/static/css/main.8a3be9e3.chunk.css"
+  },
+  {
+    "revision": "2b7928e34610b6c410a3",
+    "url": "/monstar-js-project/static/js/2.2ae8977b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstar-js-project/static/js/2.2ae8977b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb687df2cc76d85de4f0",
+    "url": "/monstar-js-project/static/js/main.d20447eb.chunk.js"
+  },
+  {
+    "revision": "b9b33f01996522bf252b",
+    "url": "/monstar-js-project/static/js/runtime-main.645c6d03.js"
+  }
+]);
